8fe1a357f1c9f7cb3321f1802bf4acea45ccf410
Joel Cheah <gjoelcheah@gmail.com>
2022-03-03 01:11:33 +0800

    add Organization model

---
+++ server/src/models/Organization.js
@@ -0,0 +1,55 @@
+import { DataTypes, Model } from '@sequelize/core';
+import sequelize from '../sequelize.js';
+import User from './User.js';
+
+class Organization extends Model {}
+
+Organization.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: 'Organization name cannot be empty',
+				},
+			},
+		},
+
+		address: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: 'Organization address cannot be empty',
+				},
+			},
+		},
+	},
+	{ sequelize, initialAutoIncrement: 1000 }
+);
+
+Organization.hasMany(User, {
+	as: 'representative',
+	foreignKey: 'orgId',
+	scope: {
+		userType: 'ORG_REPRESENTATIVE',
+	},
+});
+
+Organization.hasMany(User, {
+	as: 'applicant',
+	foreignKey: 'orgId',
+	scope: {
+		userType: 'APPLICANT',
+	},
+});
+
+User.belongsTo(Organization, { foreignKey: 'orgId' });
+export default Organization;

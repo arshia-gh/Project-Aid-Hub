abeb2811b6f200ca3e8e80af414b47b53e8d7df4
Arshia Gh <gholami.arshia@outlook.com>
2022-03-04 22:11:13 +0800

    update app.js to use api router and validation error handler

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,10 @@ import pino from 'express-pino-logger';
 import sequelize from './sequelize.js';
 sequelize.sync();
 
+import apiRoutes from './routes/api-routes.js';
+
+import { validationErrorHandler } from './middleware/error-handlers.js';
+
 import { getLoggerInstance } from './utils/logger.js';
 const logger = getLoggerInstance(import.meta.url);
 
@@ -16,6 +20,10 @@ const api_prefix = process.env.API_PREFIX || 'api';
 app.use(pino({ logger }));
 app.use(`/${api_prefix}`, express.json());
 app.use(`/${api_prefix}`, express.urlencoded({ extended: true }));
+
+app.use(`/${api_prefix}`, apiRoutes);
+
+app.use(validationErrorHandler);
 
 app.listen(sv_port, sv_host, () => {
 	logger.info(`server is listening on http://${sv_host}:${sv_port}`);

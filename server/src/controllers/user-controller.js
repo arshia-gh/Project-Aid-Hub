2d5a71f3c6f853033a1e0030344baf423fd77078
Arshia Gh <gholami.arshia@outlook.com>
2022-03-04 22:04:50 +0800

    update user controller

--- server/src/controllers/user-controller.js
+++ server/src/controllers/user-controller.js
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import _ from 'lodash';
 import User, { getNextUserId } from '../models/User.js';
 import sequelize from '../sequelize.js';
@@ -16,6 +17,39 @@ export async function getOrgRepresentatives(orgId) {
 export async function getApplicants(orgId) {
 	const foundOrganization = await findOrganizationByPk(orgId);
 	return (await foundOrganization.getApplicants()).map(toSafeUser);
+}
+
+export async function createOrgRepresentative(orgRepresentative, orgId) {
+	orgRepresentative.password = faker.internet.password(12, true);
+
+	const foundOrganization = await findOrganizationByPk(orgId);
+	const createdRep = await foundOrganization.createRepresentative(
+		orgRepresentative
+	);
+	return toSafeUser(createdRep);
+}
+
+export async function createApplicant(applicant, orgId) {
+	applicant.password = faker.internet.password(12, true);
+
+	return sequelize.transaction(async (transaction) => {
+		const foundOrganization = await findOrganizationByPk(
+			orgId,
+			transaction
+		);
+
+		const nextUserId = await getNextUserId(transaction);
+		applicant.username = generateUsername(nextUserId);
+
+		const createdApplicant = await foundOrganization.createApplicant(
+			applicant,
+			{
+				transaction,
+			}
+		);
+
+		return toSafeUser(createdApplicant);
+	});
 }
 
 export async function login(username, password) {

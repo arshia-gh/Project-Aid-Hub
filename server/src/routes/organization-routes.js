774a8a35d297e73d485c2cb92de988900988afad
Joel Cheah <gjoelcheah@gmail.com>
2022-03-04 22:44:26 +0800

    add organization routes

---
+++ server/src/routes/organization-routes.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import { promisify, validator } from '../middleware/utils-middleware.js';
+
+import {
+	createOrganization,
+	getOrganizationById,
+	getOrganizations,
+} from '../controller/organization-controller.js';
+
+import OrganizationSchema from '../schemas/organization-schema.js';
+
+const router = express.Router();
+
+router.get(
+	'/',
+	promisify(async (req, res) => {
+		const organizations = await getOrganizations();
+
+		res.status(200).json({
+			result: organizations,
+			code: 200,
+		});
+	})
+);
+
+router.get(
+	'/:orgId',
+	promisify(async (req, res) => {
+		const { orgId } = req.params;
+		const foundOrganization = await getOrganizationById(orgId);
+
+		res.status(200).json({
+			result: foundOrganization,
+			code: 200,
+		});
+	})
+);
+
+router.post(
+	'/',
+	validator.body(OrganizationSchema),
+	promisify(async (req, res) => {
+		const newOrganization = await createOrganization(req.body);
+
+		res.status(200).json({
+			result: newOrganization,
+			code: 200,
+		});
+	})
+);
+
+
+export default router;

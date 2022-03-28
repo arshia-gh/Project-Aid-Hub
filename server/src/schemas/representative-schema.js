9e518b2e4ff39524b33b2501950bf692eb791ca9
Arshia Gh <gholami.arshia@outlook.com>
2022-03-29 00:45:45 +0800

    fix #5

--- server/src/schemas/representative-schema.js
+++ server/src/schemas/representative-schema.js
@@ -4,4 +4,6 @@ import User from './user-schema.js';
 export default User.keys({
 	username: Joi.string().min(6).max(12).required(),
 	jobTitle: Joi.string().max(255).required(),
+	email: User.extract('email').required(),
+	mobileNo: User.extract('mobileNo').required(),
 });

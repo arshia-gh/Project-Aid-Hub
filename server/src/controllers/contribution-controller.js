83eb3e8e5742cb55085c784e853b2b87fb6a5a2a
Arshia Gh <gholami.arshia@outlook.com>
2022-03-29 18:08:28 +0800

    add contribution controller

---
+++ server/src/controllers/contribution-controller.js
@@ -0,0 +1,58 @@
+import Contribution from '../models/Contribution.js';
+import sequelize from '../sequelize.js';
+import ApiError from '../utils/errors.js';
+import { findAppealByPk } from './appeal-controller.js';
+
+export async function recordGoods(goods, appealId) {
+	return sequelize.transaction(async (t) => {
+		const foundAppeal = await findAppealByPk(appealId, t);
+
+		if (foundAppeal.outcome === 'ended') {
+			ApiError.badRequest(
+				'Contribution cannot be recorded for an inactive appeal'
+			);
+		}
+
+		return foundAppeal.createGood(
+			{
+				...goods,
+				contributionType: 'GOODS',
+			},
+			t
+		);
+	});
+}
+
+export async function recordCashDonation(cashDonation, appealId) {
+	return sequelize.transaction(async (t) => {
+		const foundAppeal = await findAppealByPk(appealId, t);
+
+		if (foundAppeal.outcome === 'ended') {
+			ApiError.badRequest(
+				'Contribution cannot be recorded for an inactive appeal'
+			);
+		}
+
+		return foundAppeal.createCashDonation(
+			{
+				...cashDonation,
+				contributionType: 'CASH_DONATION',
+			},
+			t
+		);
+	});
+}
+
+export async function getAllGoods(appealId) {
+	const foundAppeal = await findAppealByPk(appealId);
+	return foundAppeal.getGoods();
+}
+
+export async function getCashDonations(appealId) {
+	const foundAppeal = await findAppealByPk(appealId);
+	return foundAppeal.getCashDonations();
+}
+
+export async function getContributions(appealId) {
+	return Contribution.findAll({ where: { appealId } });
+}

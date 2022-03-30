ce0b3acaa4e32433b8488f8694086b2b0a780ee0
Joel Cheah <gjoelcheah@gmail.com>
2022-03-31 07:42:56 +0800

    add react query

--- client/src/views/representative/ViewApplicants.js
+++ client/src/views/representative/ViewApplicants.js
@@ -1,8 +1,3 @@
-import { useState, useEffect } from 'react';
-
-import axios from 'api/axios';
-
-// reactstrap components
 import {
 	Button,
 	Card,
@@ -11,110 +6,64 @@ import {
 	Container,
 	Row,
 	Badge,
-	UncontrolledAlert,
+	Spinner,
+	Col,
 } from 'reactstrap';
 
-import OrgHeader from 'components/Headers/OrganizationHeader';
 import { Link } from 'react-router-dom';
 import useAuth from 'hooks/useAuth';
-import Table from 'components/UI/Table';
-import { useAlerts } from 'hooks';
+import { OrgApplicantsTable } from 'components/DataTable';
+import { useOrgApplicantsQuery } from 'hooks';
+import ReturnButton from 'components/UI/ReturnButton';
 
 const ViewApplicants = () => {
 	const { auth } = useAuth();
-	const { addAlert } = useAlerts();
 
-	const [applicants, setApplicants] = useState([]);
-	const [organization, setOrganization] = useState({});
+	const { Organization: organization } = auth.user;
 
-	useEffect(() => {
-		let isMounted = true;
-		const controller = new AbortController();
-
-		const getApplicants = async () => {
-			try {
-				const orgResponse = await axios.get(
-					`/organizations/${auth.user.orgId}`
-				);
-				const response = await axios.get(
-					`/organizations/${auth.user.orgId}/applicants`,
-					{
-						signal: controller.signal,
-					}
-				);
-
-				isMounted && setApplicants(response.data.result);
-				isMounted && setOrganization(orgResponse.data.result);
-			} catch (err) {
-				if (err.response) {
-					const { error, code } = err.response.data;
-					addAlert({
-						title: `An ${code} error occurred`,
-						message: error.message,
-						mode: 'danger',
-					});
-				}
-			}
-		};
-
-		getApplicants();
-
-		return () => {
-			isMounted = false;
-			controller.abort();
-		};
-	}, [auth, addAlert]);
+	const {
+		isLoading,
+		isSuccess,
+		data: applicants,
+	} = useOrgApplicantsQuery(organization.id);
 
 	return (
 		<>
-			<OrgHeader organization={organization} />
 			<Container className='mt--7' fluid>
-				{auth.newApplicant && (
-					<UncontrolledAlert color='primary' fade={false}>
-						<span className='alert-inner--text'>
-							<strong>New Applicant Info</strong>
-							<br />
-							Username: {auth.newApplicant.username}
-							<br />
-							Password: {auth.newApplicant.password}
-						</span>
-					</UncontrolledAlert>
-				)}
 				<Row>
 					<div className='col'>
 						<Card className='shadow'>
 							<CardHeader className='border-0'>
 								<Row className='align-items-center'>
-									<div className='col'>
+									<Col className='d-flex align-items-center'>
+										<ReturnButton className='mr-3' />
 										<h3 className='mb-0'>
-											List of Applicants
+											{organization.name}'s Applicants
 										</h3>
-									</div>
+									</Col>
 									<div className='col text-right'>
 										<Link
 											to={
 												'/representative/new-applicant'
 											}>
 											<Button color='primary' size='sm'>
-												Add Applicant
+												<i class='fas fa-plus'></i>{' '}
+												Register Applicant
 											</Button>
 										</Link>
 									</div>
 								</Row>
 							</CardHeader>
-							<Table
-								data={applicants}
-								headers={{
-									username: 'Username',
-									fullname: 'Full Name',
-									IDno: 'ID Number',
-									householdIncome: 'Household Income',
-									address: 'Address',
-								}}
-								rowKey='username'
-								hoverable
-							/>
-							{applicants.length === 0 && (
+							{isLoading ? (
+								<div className='d-flex align-items-center ml-4 mb-4'>
+									<Badge color='primary'>
+										<Spinner />
+									</Badge>
+								</div>
+							) : (
+								<OrgApplicantsTable applicants={applicants} />
+							)}
+							{isSuccess && applicants.length === 0 && (
 								<CardFooter>
 									<Badge color='info'>
 										No applicant was found

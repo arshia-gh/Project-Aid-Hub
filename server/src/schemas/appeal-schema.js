5ab1514ab81e25acf56be03161bb045fef2d0422
Arshia Gh <gholami.arshia@outlook.com>
2022-03-30 14:35:24 +0800

    add target amount to appeal schema

--- server/src/schemas/appeal-schema.js
+++ server/src/schemas/appeal-schema.js
@@ -10,6 +10,7 @@ export default Joi.object({
 		.messages({
 			'date.greater': 'Ending date must be greater than starting date',
 		}),
+	targetAmount: Joi.number().positive().required().label('Target amount'),
 	title: Joi.string().trim().min(6).max(255).required().label('Title'),
 	description: Joi.string().trim().min(6).required().label('Description'),
 });

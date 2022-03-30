df5715f0cc4f2b2f41ba52f32346333c61f12f85
Arshia Gh <gholami.arshia@outlook.com>
2022-03-30 14:36:08 +0800

    update labels of the goods schema

--- server/src/schemas/goods-schema.js
+++ server/src/schemas/goods-schema.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
 export default Joi.object({
-	description: Joi.string().trim().max(255).required(),
+	description: Joi.string().trim().max(255).required().label('Description'),
 	estimatedValue: Joi.number().positive().required().label('Estimated value'),
 });

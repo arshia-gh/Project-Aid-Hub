093980666b1835ad4c7f9982c3b9670c74361f8f
Arshia Gh <gholami.arshia@outlook.com>
2022-04-01 20:01:54 +0800

    add reqct query hooks for use case 4, 5, 6

--- client/src/hooks/apiHooks.js
+++ client/src/hooks/apiHooks.js
@@ -1,7 +1,12 @@
 import { useQuery } from 'react-query';
 import {
 	getAppealById,
+	getAppealDisbursements,
 	getAppeals,
+	getApplicant,
+	getApplicantDisbursements,
+	getApplicantDocuments,
+	getAvailableApplicants,
 	getContributions,
 	getOrganization,
 	getOrganizations,
@@ -47,7 +52,36 @@ export const useAppealQuery = (appealId) =>
 		select: selectResult,
 	});
 
+export const useAppealDisbursementsQuery = (appealId) =>
+	useQuery(
+		['disbursements', appealId],
+		() => getAppealDisbursements(appealId),
+		{
+			select: selectResult,
+		}
+	);
+
 export const useContributionsQuery = (appealId) =>
-	useQuery(['goods', appealId], () => getContributions(appealId), {
+	useQuery(['contributions', appealId], () => getContributions(appealId), {
+		select: selectResult,
+	});
+
+export const useApplicantQuery = (IDno) =>
+	useQuery(['applicant', IDno], () => getApplicant(IDno), {
+		select: selectResult,
+	});
+
+export const useAvailableApplicantsQuery = (appealId) =>
+	useQuery(['available', appealId], () => getAvailableApplicants(appealId), {
+		select: selectResult,
+	});
+
+export const useApplicantDocumentsQuery = (IDno) =>
+	useQuery(['documents', IDno], () => getApplicantDocuments(IDno), {
+		select: selectResult,
+	});
+
+export const useApplicantDisbursements = (IDno) =>
+	useQuery(['disbursements', IDno], () => getApplicantDisbursements(IDno), {
 		select: selectResult,
 	});

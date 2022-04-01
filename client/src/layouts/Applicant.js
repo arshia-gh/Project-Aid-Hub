d49eb47eabe4f7f1f14da56d6fa0ec8b89ae6298
Arshia Gh <gholami.arshia@outlook.com>
2022-04-01 19:59:33 +0800

    add routes required for use case 5, 6 to applicant layout

--- client/src/layouts/Applicant.js
+++ client/src/layouts/Applicant.js
@@ -14,13 +14,28 @@ import DashboardNavbar from 'components/Navbars/DashboardNavbar.js';
 import DashboardFooter from 'components/Footers/DashboardFooter.js';
 import Sidebar from 'components/Sidebar/Sidebar.js';
 import Profile from 'views/applicant/Profile';
+import DashboardHeader from 'components/Headers/DashboardHeader';
+import AddDocument from 'views/applicant/AddDocument';
+import ViewDisbursements from 'views/applicant/ViewDisbursements';
 
 const routes = [
 	{
-		index: 'profile',
-		name: 'Profile',
-		icon: 'ni ni-single-02 text-yellow',
+		index: true,
+		name: 'Dashboard',
+		icon: 'fas fa-book-open text-primary',
+		element: <ViewDisbursements />,
+	},
+	{
+		path: 'profile',
+		name: 'My Profile',
+		icon: 'ni ni-single-02 text-primary',
 		element: <Profile />,
+	},
+	{
+		path: 'new-document',
+		name: 'Add Document',
+		icon: 'fas fa-file text-primary',
+		element: <AddDocument />,
 	},
 ];
 
@@ -34,7 +49,12 @@ const ApplicantLayout = () => {
 			<Sidebar routes={routes} indexLink='/applicant' />
 			<div className='main-content'>
 				<DashboardNavbar brandText={match?.route.name || 'Dashboard'} />
-				{routeResults || <Navigate to='profile' replace />}
+				<DashboardHeader />
+
+				<Container className='mt--7' fluid>
+					{routeResults || <Navigate to='profile' replace />}
+				</Container>
+
 				<Container fluid>
 					<DashboardFooter />
 				</Container>

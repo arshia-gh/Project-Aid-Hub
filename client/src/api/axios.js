69dd9e5c2d501e3b28b19535498cee535ae28c46
Arshia Gh <gholami.arshia@outlook.com>
2022-04-03 19:20:43 +0800

    update axios.js to retrieve api url from env

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiUrl = process.env.API_URL || 'http://localhost:8080/api';
+
 export const instance = axios.create({
-	baseURL: 'http://localhost:8080/api',
+	baseURL: apiUrl,
 });
 
 export const getOrganizations = async () => {
